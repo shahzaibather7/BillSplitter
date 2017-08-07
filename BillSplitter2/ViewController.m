373a17b3c88f296434a289502b30e7d168f257f5
Shahzaib Ather <shahzaibather7@github.ca>
2017-08-06 21:25:44 -0700

    committed with slider working and amount being split

--- BillSplitter2/ViewController.m
+++ BillSplitter2/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (nonatomic ) UILabel *billAmountLabel;
+@property (nonatomic ) UILabel *splitAmountLabel;
+@property (nonatomic) UITextField *textField;
+@property (nonatomic ,strong) UIGestureRecognizer *backgroundTap;
+@property (nonatomic , strong) UISlider *mySlider;
 
 @end
 
@@ -16,14 +22,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor grayColor];
+    [self createBillAmountLabel];
+    [self createTextField];
+    [self createBackgroundTapGestureRecognizer];
+    [self createSlider];
+    [self createSplitBillLabel];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) createBillAmountLabel {
+    
+    self.billAmountLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 200, 100)];
+    self.billAmountLabel.text = @"BILL AMOUNT";
+    self.billAmountLabel.font = [UIFont boldSystemFontOfSize:30];
+    [self.view addSubview:self.billAmountLabel];
+}
+-(void) createTextField {
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(100, 200, 200, 60)];
+    self.textField.backgroundColor = [UIColor whiteColor];
+    self.textField.font = [UIFont boldSystemFontOfSize:25];
+    self.textField.tag = 2;
+    self.textField.keyboardType = UIKeyboardTypeDecimalPad;
+    self.textField.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.textField];
 }
 
+-(void) createSlider {
+    self.mySlider = [[UISlider alloc]initWithFrame:CGRectMake(100, 350, 200, 20)];
+    [self.mySlider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    self.mySlider.minimumValue = 2.0;
+    self.mySlider.maximumValue = 8.0;
+    [self.view addSubview:self.mySlider];
+   }
+
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    NSLog(@"slider value = %f", sender.value);
+    self.mySlider.value = sender.value;
+    float tempFloat = [self.textField.text floatValue];
+    float tempFloat2 = self.mySlider.value;
+    self.splitAmountLabel.text = [NSString stringWithFormat:@"Split Bill Amount:$ %.2f" ,tempFloat / tempFloat2];
+    NSLog(@"%f", tempFloat2);
+    
+}
+
+-(void) createSplitBillLabel {
+    self.splitAmountLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 450, 200, 20)];
+  
+    [self.view addSubview:self.splitAmountLabel];
+}
+
+-(void)createBackgroundTapGestureRecognizer {
+    self.backgroundTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(backgroudWasTapped)];
+    [self.view addGestureRecognizer:self.backgroundTap];
+}
+-(void)backgroudWasTapped {
+    [self.view endEditing:YES];
+}
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if (textField.tag == 2){
+        NSString *newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        return !([newString length] > 3);
+    } else{
+        return true;
+    }
+}
 
 @end
